034b9f9f3edad3991b542ab8aff437e2508fe21a
Festus CODJO <xpoz8yourtalent@gmail.com>
2023-03-31 12:32:35 +0100

    tests:Add test cases for reserseArray() demonstrating Map method concept

--- src/tests/concepts/concepts.test.js
+++ src/tests/concepts/concepts.test.js
@@ -2,4 +2,8 @@ import { describe, test, expect } from 'jest'
 import { concepts } from '../../codes/concepts/concepts'
 
 describe('Concepts Tests', () => {
+  test('Map Method Tests', () => {
+    expect(concepts.reverseArray(['hello', 'world!'])).toHaveLength(2)
+    expect(concepts.reverseArray(['hello', 'world!'])).toEqual(['olleh', '!dlrow'])
+  })
 })
