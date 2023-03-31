8fbb599859475932168a2c5c9103e0ded2d9b670
Festus CODJO <xpoz8yourtalent@gmail.com>
2023-03-31 22:44:08 +0100

    tests:Add test cases for removeDuplicates() demonstrating Set|Unique values concept

---
+++ src/tests/concepts/concepts.data.js
@@ -0,0 +1,12 @@
+export const projects = [
+  { name: 'Project 1', category: 'Desktop', createdAt: Date.now() },
+  { name: 'Project 2', category: 'Desktop', createdAt: Date.now() },
+  { name: 'Project 3', category: 'Web', createdAt: Date.now() },
+  { name: 'Project 4', category: 'Web', createdAt: Date.now() },
+  { name: 'Project 5', category: 'Android', createdAt: Date.now() },
+  { name: 'Project 6', category: 'Android', createdAt: Date.now() },
+  { name: 'Project 7', category: 'iOS', createdAt: Date.now() },
+  { name: 'Project 8', category: 'iOS', createdAt: Date.now() },
+  { name: 'Project 9', category: 'iOS', createdAt: Date.now() },
+  { name: 'Project 10', category: 'Android', createdAt: Date.now() }
+]
--- src/tests/concepts/concepts.test.js
+++ src/tests/concepts/concepts.test.js
@@ -1,9 +1,16 @@
 /* eslint-disable no-undef */
 import { concepts } from '../../codes/concepts/concepts'
+import { projects } from './concepts.data'
 
 describe('Concepts Tests', () => {
   test('Map Method Tests with reverserArray()', () => {
     expect(concepts.reverseArray(['hello', 'world!'])).toHaveLength(2)
     expect(concepts.reverseArray(['hello', 'world!'])).toEqual(['olleh', '!dlrow'])
+  })
+
+  test('Set|Unique values Tests with removeDuplicates()', () => {
+    expect(concepts.removeDuplicates(['Android', 'iOS', ' Android', 'Web'])).toHaveLength(3)
+    expect(concepts.removeDuplicates(projects.map(entry => entry.category))).toHaveLength(4)
+    expect(concepts.removeDuplicates(projects.map(entry => entry.category))).toEqual(['android', 'desktop', 'ios', 'web'])
   })
 })
