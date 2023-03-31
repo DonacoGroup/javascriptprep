e97ad023642a905483ae6c9701c4a13017a0be51
Festus CODJO <xpoz8yourtalent@gmail.com>
2023-03-31 12:58:43 +0100

    feat:Add reserseArray() demonstrating Map method concept

--- src/codes/concepts/concepts.js
+++ src/codes/concepts/concepts.js
@@ -1,2 +1,4 @@
 export const concepts = {
+  // Map method returns a new array doesn't change the size of original array uses values of origin array to make new ones
+  reverseArray: (originalArray) => originalArray.map(entry => entry.split('').reverse().join(''))
 }
