ee6456959e21bc27ba604d1083bca4b7d1e485bd
Festus CODJO <xpoz8yourtalent@gmail.com>
2023-03-31 22:54:29 +0100

    feat:Add removeDuplicates() demonstrating Set|Unique values concept

--- src/codes/concepts/concepts.js
+++ src/codes/concepts/concepts.js
@@ -1,4 +1,6 @@
 export const concepts = {
   // Map method returns a new array doesn't change the size of original array uses values of origin array to make new ones
-  reverseArray: (originalArray) => originalArray.map(entry => entry.split('').reverse().join(''))
+  reverseArray: (originalArray) => originalArray.map(entry => entry.split('').reverse().join('')),
+  // Set[Unique values] return an array of unique values
+  removeDuplicates: (originalArray) => new Set((originalArray.map(entry => entry.toLowercase())).sort())
 }
