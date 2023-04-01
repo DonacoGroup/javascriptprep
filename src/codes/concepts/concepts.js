cf3620c8eae20c3de636672d3733b833a388c71e
Festus CODJO <xpoz8yourtalent@gmail.com>
2023-04-01 04:50:39 +0100

    feat:Add listProjectCatagories() for demonstrating Set|Unique values concept

--- src/codes/concepts/concepts.js
+++ src/codes/concepts/concepts.js
@@ -2,5 +2,7 @@ export const concepts = {
   // Map method returns a new array doesn't change the size of original array uses values of origin array to make new ones
   reverseArray: (originalArray) => originalArray.map(entry => entry.split('').reverse().join('')),
   // Set[Unique values] return an object of unique values
-  removeDuplicates: (originalArray) => new Set((originalArray.map(entry => entry.toLowerCase())).sort())
+  removeDuplicates: (originalArray) => new Set((originalArray.map(entry => entry.toLowerCase())).sort()),
+  // Set[Unique values] return an object of unique values
+  listProjectCategories: (originalArray) => ['all', ...new Set((originalArray.map(entry => entry.toLowerCase())).sort())]
 }
