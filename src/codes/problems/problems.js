fe2289cc6f484adc8793d4cebc976f002911593f
Festus CODJO <xpoz8yourtalent@gmail.com>
2023-04-10 23:05:00 +0100

    feat: Add tripleAdd() to solve the Triple Addition Function Problem

--- src/codes/problems/problems.js
+++ src/codes/problems/problems.js
@@ -1,2 +1,16 @@
 export const problems = {
+  tripleAdd: (firstParam) => {
+    // Contains the result of the operation
+    let result = firstParam
+    // return a function to be invoked with the secondValue
+    return (secondParam) => {
+      // Add up firstValue and secondValue
+      result += secondParam
+      // return a function to invoked with the thirdValue
+      return (thirdValue) => {
+        result += thirdValue
+        return result
+      }
+    }
+  }
 }
