c7eee54ba2e15609a56cf72d88a9118c4d98a75d
Festus CODJO <xpoz8yourtalent@gmail.com>
2023-04-10 22:53:34 +0100

    test: Add test cases for TripleAdd()

--- src/tests/problems/problems.test.js
+++ src/tests/problems/problems.test.js
@@ -2,4 +2,8 @@
 import { problems } from '../../codes/problems/problems'
 
 describe('Problems Tests', () => {
+  test('TripleAdd Function Problem', () => {
+    expect(problems.tripleAdd(10)(20)(30)).toEqual(60)
+  })
+  // test('Description', () => {})
 })
