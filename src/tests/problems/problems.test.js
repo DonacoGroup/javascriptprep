3b7801e05662a3ffc0a54a9a67ab23c06310582b
Festus CODJO <xpoz8yourtalent@gmail.com>
2023-04-11 10:25:16 +0100

    test: Add test cases for trackCalls()

--- src/tests/problems/problems.test.js
+++ src/tests/problems/problems.test.js
@@ -5,5 +5,10 @@ describe('Problems Tests', () => {
   test('TripleAdd Function Problem', () => {
     expect(problems.tripleAdd(10)(20)(30)).toEqual(60)
   })
+  test('Track Calls Function Problem', () => {
+    expect(problems.trackCalls()).toEqual(1)
+    expect(problems.trackCalls()).toEqual(2)
+    expect(problems.trackCalls()).toEqual(3)
+  })
   // test('Description', () => {})
 })
